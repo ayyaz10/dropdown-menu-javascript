2b51fe3ac6525bbb21ae8d16d67eccbe11d76b2b
ayyaz10 <codementee@outlook.com>
2022-05-23 17:13:50 +0500

    update

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ document.addEventListener('click', e => {
   if(!isDropwdownButton && e.target.closest('[data-dropdown]') != null) return;
 
   let currentDropdown;
-
   if(isDropwdownButton) {
-    console.log(isDropwdownButton)
     currentDropdown = e.target.closest('[data-dropdown]')
+    console.log(currentDropdown)
     currentDropdown.classList.toggle('active')
   }
   document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
@@ -14,5 +13,3 @@ document.addEventListener('click', e => {
     dropdown.classList.remove('active');
   })
 })
-
-// console.log('helo world')
